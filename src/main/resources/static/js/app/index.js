829dc5e5221f366b46dc74d2b2f19e3eee16a639
gony <vixvixd>
2020-08-04 21:54:33 +0900

    project

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -15,7 +15,16 @@ var main = {
         $.ajax({
             type: 'POST',
             url: '/api/v1/posts',
-
-        })
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 등록되었습니다.');
+            window.location.href = '/'
+       }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
     }
-}
+};
+
+main.init();
